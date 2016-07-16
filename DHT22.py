12d5449fa4cbbc38ecf12d8388a96158a073dd18
Nicolas Triantafillou <nicolast@gmail.com>
2016-07-16 07:18:37 +0000

    clean up some code

--- DHT22.py
+++ DHT22.py
@@ -197,30 +197,3 @@ class sensor:
       if self.cb != None:
          self.cb.cancel()
          self.cb = None
-
-if __name__ == "__main__":
-   import time
-   import pigpio
-   import DHT22
-
-   # Intervals of about 2 seconds or less will eventually hang the DHT22.
-   INTERVAL=3
-   pi = pigpio.pi()
-   s = DHT22.sensor(pi, 22, LED=16, power=8)
-   r = 0
-   next_reading = time.time()
-
-   while True:
-      r += 1
-      s.trigger()
-      time.sleep(0.2)
-      print("{} {} {} {:3.2f} {} {} {} {}".format(
-         r, s.humidity(), s.temperature(), s.staleness(),
-         s.bad_checksum(), s.short_message(), s.missing_message(),
-         s.sensor_resets()))
-
-      next_reading += INTERVAL
-      time.sleep(next_reading-time.time()) # Overall INTERVAL second polling.
-
-   s.cancel()
-   pi.stop()
